ea5ac9607db867d77de4c895d3d287488acc579d
denyncrawford <kiquerangelb@gmail.com>
2023-10-05 10:44:20 -0300

    cli:1.0.10

--- apps/cli/bin/npx.mod.js
+++ apps/cli/bin/npx.mod.js
@@ -2,13 +2,17 @@
 
 const { spawn } = require('child_process');
 const { join } = require('path');
+const { chmodSync } = require('fs');
 
 const args = process.argv.slice(2);
 const os = process.platform;
 
 const fileName = os === 'win32' ? 'cli-windows.exe' : os === 'darwin' ? 'cli-darwin' : 'cli-linux';
+const filePath = join(__dirname, '../dist', fileName);
 
-spawn(join(__dirname, '../dist', fileName), args, {
+chmodSync(filePath, 0o777);
+
+spawn(filePath, args, {
     stdio: 'inherit',
     shell: true,
     env: {
