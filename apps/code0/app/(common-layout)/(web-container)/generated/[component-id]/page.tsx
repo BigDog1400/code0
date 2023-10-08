38c565cf80417cac086a0fb70e75398f7c263f41
denyncrawford <kiquerangelb@gmail.com>
2023-10-07 23:49:44 -0300

    feat: all nice

--- apps/code0/app/(common-layout)/(web-container)/generated/[component-id]/page.tsx
+++ apps/code0/app/(common-layout)/(web-container)/generated/[component-id]/page.tsx
@@ -1,8 +1,9 @@
-'use client'; // provitional
+'use client'; // provitional, not now
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'next/navigation';
+import ComponentPreview from '@/app/_components/preview-item';
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
@@ -27,23 +28,96 @@ export default function Page() {
     }
   };
 
+  const components = [
+    {
+      prompt: 'A button component',
+      id: '0',
+    },
+    {
+      prompt: 'A table component',
+      id: '1',
+    },
+    {
+      prompt: 'A user profile component',
+      id: '2',
+    },
+    {
+      prompt: 'A navigation bar component',
+      id: '3',
+    },
+    {
+      prompt: 'A footer component',
+      id: '4',
+    },
+    {
+      prompt: 'A spotify player component',
+      id: '5',
+    },
+    {
+      prompt: 'A spotify player component',
+      id: '6',
+    },
+    {
+      prompt: 'A spotify player component',
+      id: '7',
+    },
+    {
+      prompt: 'A spotify player component',
+      id: '8',
+    },
+    {
+      prompt: 'A spotify player component',
+      id: '9',
+    }
+  ]
+
+  const [selectedItem, setSelectedItem] = useState<{ prompt: string, id: string } | null>(components[0]);
+  const [scrollIndex, setScrollIndex] = useState<number>(0);
+
+  useEffect(() => {
+    if (selectedItem) {
+      const selectedIndex = components.findIndex((item) => item.id === selectedItem.id);
+      if (selectedIndex >= 2) {
+        setScrollIndex(selectedIndex - 2);
+      }
+    }
+  }, [selectedItem, components]);
+
+  const handleItemClick = (item: { prompt: string, id: string }) => {
+    setSelectedItem(item);
+  };
+
+  const visiblecomponents = useMemo(() => {
+    const startIndex = Math.min(scrollIndex, components.length - 5);
+    return components.slice(startIndex, startIndex + 5);
+  }, [scrollIndex, components]);
+
+  const handleScroll = (direction: 'up' | 'down') => {
+    const scrollStep = direction === 'up' ? -1 : 1;
+    const newScrollIndex = Math.min(
+      Math.max(scrollIndex + scrollStep, 0),
+      components.length - 5
+    );
+    setScrollIndex(newScrollIndex);
+  };
+
   return (
     <>
-      {/* <main
-        id="generated"
-        className="flex flex-col items-center justify-between p-24"
-      > */}
-      {/* <h1>Hello, Dashboard Page!</h1>
-        <Link href="/">Go to Form</Link>
-        <Link href="/generated/vyZCDDT27nC9kLhdRNKCZ">Go to Component A</Link>
-        <Link href="/generated/97hNKK7seFTLOjKKoFt8F">Go to Component B</Link> */}
-      {/* </main> */}
-      {/* <div>{imagePreview && <img src={imagePreview} />}</div>
-      <div>
-        <button onClick={() => getImagePreview()}>
-          {isImagePreviewLoading ? 'Loading...' : 'Get Image Preview'}
-        </button>
-      </div> */}
+      <div className="h-full min-w-[200px] flex flex-col justify-center px-4 absolute right-0">
+        <div className='flex flex-col gap-2 transition-all snap-mandatory snap-y max-h-[532px] no-scrollbar'>
+          {visiblecomponents.map((component, index) => (
+            <ComponentPreview
+              key={component.id}
+              isSelected={selectedItem?.id === component.id}
+              image="/api/preview/gateway/bafybeieprauiv2cdumlkoydzlnvmyqhbwh5vsprbf3igknsuhqqwbfkzlu/Macarena_Rivas_V._64f743b304c18cbae9964cbd__invitation.png"
+              action={() => {
+                handleItemClick(component);
+              }}
+              prompt={component.prompt}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- apps/code0/app/(common-layout)/(web-container)/layout.tsx
+++ apps/code0/app/(common-layout)/(web-container)/layout.tsx
@@ -10,7 +10,7 @@ export default function WebContainerLayout({
     <div className="relative">
       {children}
       <div className="flex items-center justify-center h-[calc(100vh-6rem)]">
-        <div className="w-[80vw] h-[80vh] p-[14px] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl overflow-hidden relative">
+        <div className="w-[79vw] h-[80vh] p-[14px] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl overflow-hidden relative">
           <div className="w-full h-full border-[2px] border-black/20 rounded-xl bg-white overflow-hidden">
             <WebContainerClient />
           </div>
